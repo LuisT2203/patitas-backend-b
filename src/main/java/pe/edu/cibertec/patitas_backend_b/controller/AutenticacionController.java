909d05915b91b2c7aae53b045eed7c6182a12462
luis <luistran22@gmail.com>
2024-10-12 22:53:10 -0500

    t2 backend

--- src/main/java/pe/edu/cibertec/patitas_backend_b/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend_b/controller/AutenticacionController.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.cibertec.patitas_backend_b.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_backend_b.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_backend_b.dto.LogoutRequestDTO;
+import pe.edu.cibertec.patitas_backend_b.dto.LogoutResponseDTO;
 import pe.edu.cibertec.patitas_backend_b.service.AutenticacionService;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/autenticacion")
@@ -20,25 +23,44 @@ public class AutenticacionController {
     AutenticacionService autenticacionService;
 
     @PostMapping("/login")
-    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO){
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
         try {
             Thread.sleep(Duration.ofSeconds(5));//tiempo en que va a devolver la respuesta
-            String [] datosUsuario =autenticacionService.validarUsuario(loginRequestDTO);
-            System.out.println("Respuesta backend : "+ Arrays.toString(datosUsuario));
-            if(datosUsuario==null){
-                return  new LoginResponseDTO("01","Error: Usuario no encontrado","","");
+            String[] datosUsuario = autenticacionService.validarUsuario(loginRequestDTO);
+            System.out.println("Respuesta backend : " + Arrays.toString(datosUsuario));
 
-            }else {
-                return  new LoginResponseDTO("00","",datosUsuario[0],datosUsuario[1]);
+            if (datosUsuario == null) {
+                return new LoginResponseDTO("01", "Error: Usuario no encontrado", "", "","","");
+
+            } else {
+                return new LoginResponseDTO("00", "", datosUsuario[0], datosUsuario[1],datosUsuario[2],datosUsuario[3]);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return  new LoginResponseDTO("99","Error: Ocurrio un problema","","");
+            return new LoginResponseDTO("99", "Error: Ocurrio un problema", "", "","","");
 
 
         }
 
-
-
     }
+
+    @PostMapping("/logout")
+    public LogoutResponseDTO logout(@RequestBody LogoutRequestDTO logoutRequestDTO) {
+        try {
+            Thread.sleep(Duration.ofSeconds(5));//tiempo en que va a devolver la respuesta
+            Date fechaLogout = autenticacionService.cerrarSesion(logoutRequestDTO);
+            System.out.println("Respuesta backend : " + fechaLogout);
+
+            if (fechaLogout == null) {
+                return new LogoutResponseDTO(false, null, "Error: No se pudo registrar auditoria");
+            }
+            return new LogoutResponseDTO(true,fechaLogout,"Sesion Cerrada Con Exito");
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new LogoutResponseDTO(false, null, "Error: Ocurrio un problema");
+        }
+    }
+
 }
--- src/main/java/pe/edu/cibertec/patitas_backend_b/service/impl/AutenticacionServiceImpl.java
+++ src/main/java/pe/edu/cibertec/patitas_backend_b/service/impl/AutenticacionServiceImpl.java
@@ -6,11 +6,18 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import pe.edu.cibertec.patitas_backend_b.dto.LoginRequestDTO;
+import pe.edu.cibertec.patitas_backend_b.dto.LogoutRequestDTO;
 import pe.edu.cibertec.patitas_backend_b.service.AutenticacionService;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Date;
 
 @Service
 public class AutenticacionServiceImpl implements AutenticacionService {
@@ -32,9 +39,12 @@ public class AutenticacionServiceImpl implements AutenticacionService {
                         loginRequestDTO.numeroDocumento().equals(datos[1]) &&
                         loginRequestDTO.password().equals(datos[2])){
 
-                    datosUsuario = new String[2];
-                    datosUsuario[0] = datos[3];//recupera nombre
-                    datosUsuario[1] = datos[4];//recupera email
+                    datosUsuario = new String[4];
+
+                    datosUsuario[0] = datos[0]; //recupera el tipodeDocumento
+                    datosUsuario[1] = datos[1];//recupera el numerodeDocumento
+                    datosUsuario[2] = datos[3];//recupera nombre
+                    datosUsuario[3] = datos[4];//recupera el email
                 }
             }
 
@@ -46,5 +56,37 @@ public class AutenticacionServiceImpl implements AutenticacionService {
 
 
         return datosUsuario;
+    }
+
+    @Override
+    public Date cerrarSesion(LogoutRequestDTO logoutRequestDTO) throws IOException {
+
+
+        Date fechaLogout = null;
+        Resource resource = resourceLoader.getResource("classpath:auditoria.txt");
+        Path rutaArchivo = Paths.get(resource.getURI());
+
+        try(BufferedWriter bw = Files.newBufferedWriter(rutaArchivo, StandardOpenOption.APPEND)){
+
+            //definir fecha
+            fechaLogout = new Date();
+            //preparar linea
+            StringBuilder sb = new StringBuilder();
+            sb.append(logoutRequestDTO.tipoDocumento());
+            sb.append(";");
+            sb.append(logoutRequestDTO.numeroDocumento());
+            sb.append(";");
+            sb.append(new Date());
+
+            //escribir linea
+            bw.write(sb.toString());
+            bw.newLine();
+            System.out.println(sb.toString());
+        }catch(IOException e){
+            fechaLogout = null;
+            throw new IOException(e);
+        }
+
+        return fechaLogout;
     }
 }
